0febb4dbcb04d9cc4ec604e401f99e562707c337
Andrey Bernardo <rochaandrey157@gmail.com>
2025-03-11 17:42:00 -0300

    start

---
+++ src/main/java/com/boot/spring/curso/Cadastro/de/Objetos/CadastroDeObjetosApplication.java
@@ -0,0 +1,13 @@
+package com.boot.spring.curso.Cadastro.de.Objetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeObjetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeObjetosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boot/spring/curso/Cadastro/de/Objetos/CadastroDeObjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.spring.curso.Cadastro.de.Objetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeObjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
